85bc4c94346daeab5d50e45a90c89e23b299fb3d
sparkiscool <73146902+sparkiscool@users.noreply.github.com>
2021-04-15 19:09:14 -0700

    Add files via upload

---
+++ bloque.js
@@ -0,0 +1,24 @@
+class bloque{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, 50,60, options);
+        this.width = 50;
+        this.height = 60;
+        
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+}
+//bippity boppity
---
+++ pewpew.js
@@ -0,0 +1,22 @@
+class pewpew{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+     line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.pointB.x,this.pointB.y)
+    
+}}
